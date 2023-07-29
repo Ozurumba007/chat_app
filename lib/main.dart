adbbaf3215f766f4f10a077b97d580d80e12b60f
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-07-29 18:50:53 +0100

    copied the login page scaffold code to the register page \ added confirm password to the register page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/pages/register_page.dart';
 import 'package:flutter/material.dart';
 
 import 'pages/login_page.dart';
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: RegisterPage(),
     );
   }
 }
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../components/my_button.dart';
+import '../components/my_text_field.dart';
+
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
 
@@ -8,8 +11,94 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  // text controllers
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 25),
+                // logo
+                Icon(
+                  Icons.message,
+                  size: 100,
+                  color: Colors.grey[800],
+                ),
+                const SizedBox(height: 25),
+
+                // Create an account text
+
+                const Text(
+                  'Let\'s create an account for you!',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(height: 25),
+
+                // email textfield
+
+                MyTextField(
+                  controller: emailController,
+                  hintText: 'Email',
+                  obscureText: false,
+                ),
+                const SizedBox(height: 10),
+
+                // password textfield
+
+                MyTextField(
+                  controller: passwordController,
+                  hintText: 'Password',
+                  obscureText: true,
+                ),
+                const SizedBox(height: 10),
+
+                // confirm password textfield
+
+                MyTextField(
+                  controller: confirmPasswordController,
+                  hintText: 'Confirm Password',
+                  obscureText: true,
+                ),
+
+                const SizedBox(height: 25),
+
+                // sign in button
+                MyButton(onTap: () {}, text: 'Sign In'),
+
+                const SizedBox(height: 50),
+
+                // don't have an account? register now
+
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Don\'t have an account?'),
+                    SizedBox(width: 4),
+                    Text(
+                      'Register now',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
