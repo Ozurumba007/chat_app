e197998f516689009948b83767b2f2a948622285
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-07-30 19:25:30 +0100

    created a file called auth service.dart in the auth folder \ added the configuration for users to signIn using email and password

---
+++ lib/services/auth/auth_service.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  // instance of auth
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  // sign user in
+  Future<UserCredential> signInEmailandPassword(
+      String email, String password) async {
+    try {
+      // sign in
+      UserCredential userCredential =
+          await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential;
+    }
+    // catch any errors
+    on FirebaseAuthException catch (e) {
+      throw Exception(e.code);
+    }
+  }
+
+  // sign user out
+}
