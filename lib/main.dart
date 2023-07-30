e2c7458b52db437e6242dbdbc7011851b8b1ea17
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-07-30 19:17:53 +0100

    initialized firebase in the main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
-import 'package:chat_app/services/auth/login_or_register.dart';
+import 'package:chat_app/firebase_options.dart';
+import 'package:chat_app/services/auth/auth_gate.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -10,9 +14,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginOrRegister(),
+      home: AuthGate(),
     );
   }
 }
