c94d13412a5fbacc36645af2b2adf9eb2fa8b3a5
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-07-31 13:51:49 +0100

    created the message input

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import '../components/my_text_field.dart';
+import '../services/chat/chat_service.dart';
 
 class ChatPage extends StatefulWidget {
   final String receiverUserEmail;
@@ -14,6 +18,22 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  final TextEditingController _messageController = TextEditingController();
+  final ChatService _chatService = ChatService();
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  void sendMessage() async {
+    if (_messageController.text.isNotEmpty) {
+      // only send message if there is something to send
+
+      await _chatService.sendMessage(
+          widget.receiverUserID, _messageController.text);
+
+      // clear controller after send message
+      _messageController.clear();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,6 +42,46 @@ class _ChatPageState extends State<ChatPage> {
           widget.receiverUserEmail,
         ),
       ),
+      body: Column(
+        children: [
+          // messages
+          Expanded(
+            child: _buildMessageList(),
+          ),
+
+          // user input
+          _buildMessageInput(),
+        ],
+      ),
+    );
+  }
+
+  // build message list
+
+  // build message item
+
+  // build message input
+  Widget _buildMessageInput() {
+    return Row(
+      children: [
+        // textfield
+        Expanded(
+          child: MyTextField(
+            controller: _messageController,
+            hintText: 'Enter message',
+            obscureText: false,
+          ),
+        ),
+
+        // send button
+        IconButton(
+          onPressed: sendMessage,
+          icon: const Icon(
+            Icons.send,
+            size: 40,
+          ),
+        ),
+      ],
     );
   }
 }
