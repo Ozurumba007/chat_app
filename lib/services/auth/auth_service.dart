38f2a908a3aff2f46946a492af6f4ed65b1b24cb
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-07-31 11:52:51 +0100

    added cloud fire store to the application for saing data \ added multidex to be true \ changed the minimal sdk to be 19 because of the update of firestore

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,9 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class AuthService extends ChangeNotifier {
   // instance of auth
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  // instance of firestore
+  final FirebaseFirestore _fireStore = FirebaseFirestore.instance;
 
   // sign user in
   Future<UserCredential> signInEmailandPassword(
@@ -15,6 +19,12 @@ class AuthService extends ChangeNotifier {
         email: email,
         password: password,
       );
+
+      // add a document for the user in users collection if it doesn't already exists
+      _fireStore.collection('users').doc(userCredential.user!.uid).set({
+        "uid": userCredential.user!.uid,
+        "email": email,
+      }, SetOptions(merge: true));
       return userCredential;
     }
     // catch any errors
@@ -32,6 +42,12 @@ class AuthService extends ChangeNotifier {
         email: email,
         password: password,
       );
+
+      // after creating the user, create a new document for the user in the users collection
+      _fireStore.collection('users').doc(userCredential.user!.uid).set({
+        "uid": userCredential.user!.uid,
+        "email": email,
+      });
       return userCredential;
     } on FirebaseAuthException catch (e) {
       throw Exception(e.code);
