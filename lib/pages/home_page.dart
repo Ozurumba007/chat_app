a0ba9da1d3196aa3f47f7567947cc0091d8178fa
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-07-31 12:24:12 +0100

    added a buildUserlist except for current user \ added a build individual user list items \ added a display users except current user

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:chat_app/services/auth/auth_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -10,6 +12,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // instance of auth
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   // sign user out
 
   void signOut() async {
@@ -35,6 +39,43 @@ class _HomePageState extends State<HomePage> {
           )
         ],
       ),
+      body: _buildUserList(),
     );
+  }
+
+  // build a list of users except for the current logged in user
+  Widget _buildUserList(DocumentSnapshot document) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('users').snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Text('error');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text('Loading ...');
+        }
+
+        return ListView(
+          children: snapshot.data!.docs
+              .map<Widget>((doc) => _buildUserListItem(doc))
+              .toList(),
+        );
+      },
+    );
+  }
+
+  // build individual user list items
+  Widget _buildUSerListItem(DocumentSnapshot document) {
+    Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+
+    // display alll users except current user
+
+    if (_auth.currentUser!.email != data['email']) {
+      return ListTile(
+        title: data['email'],
+        onTap: () {},
+      );
+    }
   }
 }
